5380024a513ad5fdb0d8e9af588df9cb1bca8247
Hampus Serneke <hampus.serneke@gmail.com>
2022-07-10 00:53:03 +0200

    a

--- image_analysis.py
+++ image_analysis.py
@@ -1,11 +1,41 @@
 import cv2
 import pytesseract
+import numpy as np
 
-def analyze_frame(frame):
+def rotate_image(mat, angle):
+    """
+    Rotates an image (angle in degrees) and expands image to avoid cropping
+    """
 
-    threshold = 170
+    height, width = mat.shape[:2] # image shape has 3 dimensions
+    image_center = (width/2, height/2) # getRotationMatrix2D needs coordinates in reverse order (width, height) compared to shape
+
+    rotation_mat = cv2.getRotationMatrix2D(image_center, angle, 1.)
+
+    # rotation calculates the cos and sin, taking absolutes of those.
+    abs_cos = abs(rotation_mat[0,0]) 
+    abs_sin = abs(rotation_mat[0,1])
+
+    # find the new width and height bounds
+    bound_w = int(height * abs_sin + width * abs_cos)
+    bound_h = int(height * abs_cos + width * abs_sin)
+
+    # subtract old image center (bringing image back to origo) and adding the new image center coordinates
+    rotation_mat[0, 2] += bound_w/2 - image_center[0]
+    rotation_mat[1, 2] += bound_h/2 - image_center[1]
+
+    # rotate image with the new bounds and translated rotation matrix
+    rotated_mat = cv2.warpAffine(mat, rotation_mat, (bound_w, bound_h))
+    return rotated_mat
+
+
+
+def analyze_frame(frame, angle=0):
+
+    threshold = 200
     gray = True
-    black_and_white = False
+    black_and_white = True
+    angle = -26
 
     # Spinna?
     # Högre upplösning
@@ -17,17 +47,48 @@ def analyze_frame(frame):
         (thresh, blackAndWhiteImage) = cv2.threshold(grayImage, threshold, 255, cv2.THRESH_BINARY)
 
         if black_and_white:
-            cv2.imshow('BlackAndWhite', blackAndWhiteImage)
-            text = pytesseract.image_to_string(blackAndWhiteImage)
+            img = rotate_image(blackAndWhiteImage, angle)
+
+            # norm_img = np.zeros((img.shape[0], img.shape[1]))
+            # img = cv2.normalize(img, norm_img, 0, 255, cv2.NORM_MINMAX)
+            # img = cv2.threshold(img, 100, 255, cv2.THRESH_BINARY)[1]
+            #img = cv2.GaussianBlur(img, (1, 1), 0)
+
+
+            #img = cv2.bilateralFilter(img, 15*2, 75, 2*75)
+            img = cv2.medianBlur(img, 1)
+
+            # cv2.imwrite("test_images/test.png", img)
+            # img = cv2.imread("test_images/entire.png")
+
+            cv2.imshow('BlackAndWhite', img)
+            text = pytesseract.image_to_string(img)
+
+            print(text)
 
         else:
-            cv2.imshow('JustGray', grayImage)
-            text = pytesseract.image_to_string(grayImage)
+            img = rotate_image(grayImage, angle)
+            cv2.imshow('Gray', img)
+            text = pytesseract.image_to_string(img)
+
+            print(text)
         
-        print(text)
     else:
-        cv2.imshow('Plain', frame)
-        text = pytesseract.image_to_string(frame)
+
+        img = frame
+
+        #img = cv2.bilateralFilter(img, 15, 75, 75)
+        #img = cv2.GaussianBlur(img, (1, 1), 1)
+        #img = cv2.medianBlur(img,5)
+
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        img = cv2.Canny(gray, 20*4*2*2, 30*4*2*2)
+
+        img = rotate_image(img, angle)
+
+        cv2.imshow('Plain', img)
+        text = pytesseract.image_to_string(img)
         
         print(text)
 
--- program.py
+++ program.py
@@ -54,11 +54,15 @@ def play_codes(path):
     fps = int(cap.get(cv2.CAP_PROP_FPS))
     duration = round(frames / fps)
 
-    timestamps_A = [(2903, 2922), (4752, 4770), (5899, 5918), (7927, 7945), (8430, 8448), (9693, 9711),
+    timestamps_650 = [(2903, 2922), (4752, 4770), (5899, 5918), (7927, 7945), (8430, 8448), (9693, 9711),
     (11952, 11971), (13302, 13321), (16442, 16461), (16672, 16691), (20049, 20067),
     (22180, 22200), (22440, 22459), (25262, 25281), (27256, 27275), (30801, 30820)]
+
+    timestamps_500 = []
+
+    timestamps = timestamps_650
  
-    for code_interval in timestamps_A:
+    for code_interval in timestamps:
         for frame_number in range(code_interval[0], code_interval[1] + 1):
             cap.set(cv2.CAP_PROP_POS_FRAMES, frame_number)
             returned, frame = cap.read()
@@ -133,6 +137,81 @@ def play_sound(path):
     cv2.destroyAllWindows()
 
 
+
+def rotate_image_crop(image, angle):
+  image_center = tuple(np.array(image.shape[1::-1]) / 2)
+  rot_mat = cv2.getRotationMatrix2D(image_center, angle, 1.0)
+  result = cv2.warpAffine(image, rot_mat, image.shape[1::-1], flags=cv2.INTER_LINEAR)
+  return result
+
+
+def rotate_image(mat, angle):
+    """
+    Rotates an image (angle in degrees) and expands image to avoid cropping
+    """
+
+    height, width = mat.shape[:2] # image shape has 3 dimensions
+    image_center = (width/2, height/2) # getRotationMatrix2D needs coordinates in reverse order (width, height) compared to shape
+
+    rotation_mat = cv2.getRotationMatrix2D(image_center, angle, 1.)
+
+    # rotation calculates the cos and sin, taking absolutes of those.
+    abs_cos = abs(rotation_mat[0,0]) 
+    abs_sin = abs(rotation_mat[0,1])
+
+    # find the new width and height bounds
+    bound_w = int(height * abs_sin + width * abs_cos)
+    bound_h = int(height * abs_cos + width * abs_sin)
+
+    # subtract old image center (bringing image back to origo) and adding the new image center coordinates
+    rotation_mat[0, 2] += bound_w/2 - image_center[0]
+    rotation_mat[1, 2] += bound_h/2 - image_center[1]
+
+    # rotate image with the new bounds and translated rotation matrix
+    rotated_mat = cv2.warpAffine(mat, rotation_mat, (bound_w, bound_h))
+    return rotated_mat
+
+
+
+def analyze_codes_rotated(path):
+    cap = cv2.VideoCapture(path)
+
+    frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    fps = int(cap.get(cv2.CAP_PROP_FPS))
+    duration = round(frames / fps)
+
+    timestamps_650 = [(2903, 2922), (4752, 4770), (5899, 5918), (7927, 7945), (8430, 8448), (9693, 9711),
+    (11952, 11971), (13302, 13321), (16442, 16461), (16672, 16691), (20049, 20067),
+    (22180, 22200), (22440, 22459), (25262, 25281), (27256, 27275), (30801, 30820)]
+
+    timestamps_500 = []
+
+    timestamps_selected = [(11952, 11971)]
+
+    timestamps = timestamps_selected
+ 
+    for code_interval in timestamps:
+        for frame_number in range(code_interval[0], code_interval[1] + 1):
+            cap.set(cv2.CAP_PROP_POS_FRAMES, frame_number)
+            returned, frame = cap.read()
+
+            if returned:
+                #cv2.imshow('Frame', frame)
+                #print(frame_number)
+
+                #rotated_frame = rotate_image(frame, -25)
+                image_analysis.analyze_frame(frame, angle=-25)
+                print(frame_number)
+
+                # Press N on keyboard to go to next frame
+                while not ((cv2.waitKey(12) & 0xFF == ord('n'))):
+                    pass
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+
 def analyze_video(path):
     cap = cv2.VideoCapture(path)
 
@@ -194,7 +273,9 @@ def main():
 
     #play_sound(video_path)
 
-    analyze_codes(video_path)
+    #analyze_codes(video_path)
+
+    analyze_codes_rotated(video_path)
 
 
 if __name__ == "__main__":
