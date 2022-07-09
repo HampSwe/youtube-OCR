45035dc6e9f4e0ba39bb911677923072eba59cfa
Hampus Serneke <hampus.serneke@gmail.com>
2022-07-09 19:55:51 +0200

    a

--- image_analysis.py
+++ image_analysis.py
@@ -1,7 +1,13 @@
 import cv2
 
 def analyze_frame(frame):
-    cv2.imshow('Frame', frame)
+
+    threshold = 210
+
+    grayImage = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    (thresh, blackAndWhiteImage) = cv2.threshold(grayImage, threshold, 255, cv2.THRESH_BINARY)
+
+    cv2.imshow('BlackAndWhite', blackAndWhiteImage)
 
     
 
