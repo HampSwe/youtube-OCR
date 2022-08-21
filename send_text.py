58a50d68d98acdd38f61578777097c00074f7f53
Hampus Serneke <46841359+HampSwe@users.noreply.github.com>
2022-08-21 11:41:19 +0200

    Update send_text.py
    Remove unnecessary line

--- send_text.py
+++ send_text.py
@@ -1,6 +1,6 @@
 from twilio.rest import Client
 
-client = Client("AC98769180bc1c24a170f068c8b03b59d1", "56f6f56a9441de1fea5c7e59addb72f7")
+client = Client("", "")
 
 to_number = "+46708792939"
 from_number = "+19705509127"
